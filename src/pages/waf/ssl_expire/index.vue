d034a58583e07f5805317987cd8603756f445d50
samwaf <samwafgo@gmail.com>
2025-01-03 14:20:03 +0800

    fix:default list

--- src/pages/waf/ssl_expire/index.vue
+++ src/pages/waf/ssl_expire/index.vue
@@ -306,7 +306,7 @@ export default Vue.extend({
           console.log(resdata)
           if (resdata.code === 0) {
 
-            this.data = resdata.data.list;
+            this.data = resdata.data.list??[];
             this.data_attach = []
             console.log('getList',this.data)
             this.pagination = {
