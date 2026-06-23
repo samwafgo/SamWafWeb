c7cfc3ffe2789dc1d0d5f5293b429e2e0cd4b2e7
samwaf <samwafgo@gmail.com>
2026-06-23 14:45:13 +0800

    fix: refine application page i18n wording

--- src/i18n/zh_CN.js
+++ src/i18n/zh_CN.js
@@ -995,7 +995,6 @@ export default {
         add_header: '添加头信息',
         insert_variable: '插入变量',
       },
-      tab_custom_response_headers: "自定义响应头",
       custom_response_headers: {
         is_enable: '是否开启自定义响应头',
         is_enable_tips: '开启后将在返回给客户端的响应中添加自定义的HTTP头信息',
@@ -2621,7 +2620,6 @@ export default {
       delete_failed: "删除失败",
       delete_confirm: "确认删除该订阅吗？",
       already_subscribed: "该渠道已订阅此消息类型",
-      batch_add_success: "批量添加成功，共添加 {count} 个订阅",
       batch_add_partial_success: "部分添加成功，成功 {success} 个，失败 {failed} 个",
       select_message_types: "请至少选择一个消息类型",
       channel_placeholder: "请选择通知渠道",
